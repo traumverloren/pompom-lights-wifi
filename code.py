a82c916ebae603685e3ec0e57a7a3cd873f48f9d
Stephanie Nemeth <stephanie@hey.com>
2022-01-03 20:41:41 +0100

    add on/off code requests

--- code.py
+++ code.py
@@ -1,13 +1,12 @@
-import ipaddress
+import time
 import ssl
 import wifi
 import socketpool
 import adafruit_requests
-
-# URLs to fetch from
-TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
-JSON_QUOTES_URL = "https://www.adafruit.com/api/quotes.php"
-JSON_STARS_URL = "https://api.github.com/repos/adafruit/circuitpython"
+import board
+import neopixel
+import digitalio
+import touchio
 
 # Get wifi details and more from a secrets.py file
 try:
@@ -16,45 +15,61 @@ except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-print("ESP32-S2 WebClient Test")
+# Remove these two lines on boards without board.NEOPIXEL_POWER.
+np_power = digitalio.DigitalInOut(board.NEOPIXEL_POWER)
+np_power.switch_to_output(value=False)
+np = neopixel.NeoPixel(board.NEOPIXEL, 1)
+np_power.value = True
+np[0] = (255, 0, 0)
+time.sleep(2)
+np[0] = (0, 0, 0)
 
-print("My MAC addr:", [hex(i) for i in wifi.radio.mac_address])
+touch_A2 = touchio.TouchIn(board.A2)  # on
+touch_TX = touchio.TouchIn(board.TX)  # off
 
-print("Available WiFi networks:")
-for network in wifi.radio.start_scanning_networks():
-    print("\t%s\t\tRSSI: %d\tChannel: %d" % (str(network.ssid, "utf-8"),
-                                             network.rssi, network.channel))
-wifi.radio.stop_scanning_networks()
+touch_sensors = [touch_TX, touch_A2]
+
+HUE_URL = 'http://' + secrets["ip_address"] + \
+    '/api/' + secrets["api_id"] + '/groups/12/action'
+
+new_color = ''
+is_touched = False
 
 print("Connecting to %s" % secrets["ssid"])
 wifi.radio.connect(secrets["ssid"], secrets["password"])
 print("Connected to %s!" % secrets["ssid"])
 print("My IP address is", wifi.radio.ipv4_address)
 
-ipv4 = ipaddress.ip_address("8.8.4.4")
-print("Ping google.com: %f ms" % (wifi.radio.ping(ipv4)*1000))
+np[0] = (0, 255, 0)
+time.sleep(2)
+np[0] = (0, 0, 0)
 
 pool = socketpool.SocketPool(wifi.radio)
 requests = adafruit_requests.Session(pool, ssl.create_default_context())
 
-print("Fetching text from", TEXT_URL)
-response = requests.get(TEXT_URL)
-print("-" * 40)
-print(response.text)
-print("-" * 40)
+while True:
+    if touch_TX or touch_A2:
+        is_touched = True
+        time.sleep(1)
 
-print("Fetching json from", JSON_QUOTES_URL)
-response = requests.get(JSON_QUOTES_URL)
-print("-" * 40)
-print(response.json())
-print("-" * 40)
+    if is_touched:
+        if touch_A2.value:  # on
+            print("A2 touched!")
+            np[0] = (50, 50, 50)
+            time.sleep(2)
+            np[0] = (0, 0, 0)
+            data = '{{"scene":"{}"}}'.format(secrets["on_scene"])
+        if touch_TX.value:  # off
+            print("TX touched!")
+            np[0] = (90, 0, 127)
+            time.sleep(2)
+            np[0] = (0, 0, 0)
+            data = '{"on":false}'
 
-print()
-
-print("Fetching and parsing json from", JSON_STARS_URL)
-response = requests.get(JSON_STARS_URL)
-print("-" * 40)
-print("CircuitPython GitHub Stars", response.json()["stargazers_count"])
-print("-" * 40)
-
-print("done")
+            print(new_color + '!!!!')
+            print("PUTing data to {0}".format(HUE_URL))
+            response = requests.put(HUE_URL, data=data)
+            print("-" * 40)
+            print(response.status_code)
+            print("-" * 40)
+            response.close()
