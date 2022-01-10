566374bf476bd150927127026ef63782376a927d
Stephanie Nemeth <stephanie@hey.com>
2022-01-10 21:19:51 +0100

    Fix connectivity errors and refactor

--- code.py
+++ code.py
@@ -7,6 +7,13 @@ import board
 import neopixel
 import digitalio
 import touchio
+import ipaddress
+
+OFF = (0, 0, 0)
+RED = (255, 0, 0)
+WHITE = (50, 50, 50)
+GREEN = (0, 255, 0)
+PURPLE = (90, 0, 127)
 
 # Get wifi details and more from a secrets.py file
 try:
@@ -20,9 +27,9 @@ np_power = digitalio.DigitalInOut(board.NEOPIXEL_POWER)
 np_power.switch_to_output(value=False)
 np = neopixel.NeoPixel(board.NEOPIXEL, 1)
 np_power.value = True
-np[0] = (255, 0, 0)
+np[0] = RED
 time.sleep(2)
-np[0] = (0, 0, 0)
+np[0] = OFF
 
 touch_A2 = touchio.TouchIn(board.A2)  # on
 touch_TX = touchio.TouchIn(board.TX)  # off
@@ -30,7 +37,7 @@ touch_TX = touchio.TouchIn(board.TX)  # off
 touch_sensors = [touch_TX, touch_A2]
 
 for sensor in touch_sensors:
-    sensor.threshold = 30000
+    sensor.threshold = 26000
 
 HUE_URL = 'http://' + secrets["ip_address"] + \
     '/api/' + secrets["api_id"] + '/groups/12/action'
@@ -40,7 +47,7 @@ wifi.radio.connect(secrets["ssid"], secrets["password"])
 print("Connected to %s!" % secrets["ssid"])
 print("My IP address is", wifi.radio.ipv4_address)
 
-np[0] = (0, 255, 0)
+np[0] = GREEN
 time.sleep(2)
 np[0] = (0, 0, 0)
 
@@ -61,25 +68,30 @@ while True:
     if is_touched:
         if touch_A2.value:  # on
             print("A2 touched!")
-            np[0] = (50, 50, 50)
-            time.sleep(2)
-            np[0] = (0, 0, 0)
             data = '{{"scene":"{}"}}'.format(secrets["on_scene"])
             new_state = "on"
         if touch_TX.value:  # off
             print("TX touched!")
-            np[0] = (90, 0, 127)
-            time.sleep(2)
-            np[0] = (0, 0, 0)
             data = '{"on":false}'
             new_state = "off"
 
         if current_state != new_state:
-            is_touched = False
-            current_state = new_state
+            np[0] = WHITE if new_state == "on" else PURPLE
+            print(data)
             print("PUTing data to {0}".format(HUE_URL))
             response = requests.put(HUE_URL, data=data)
-            print("-" * 40)
-            print(response.status_code)
-            print("-" * 40)
             response.close()
+            is_touched = False
+            current_state = new_state
+            time.sleep(2)
+            np[0] = OFF
+
+    # else:
+    #     test_ping = wifi.radio.ping(
+    #         ipaddress.ip_address(secrets["ip_address"]))
+    #     print(test_ping)
+
+    #     if test_ping == 0:
+    #         wifi.radio.connect(secrets["ssid"], secrets["password"])
+    #         print("Reconnected to %s!" % secrets["ssid"])
+    #     time.sleep(0.5)
